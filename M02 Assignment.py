bd17142987e1d0b0e12a9e88afa4a39dc8b051c6
Martinique Akinyemi <martinique.yvette@gmail.com>
2026-03-31 08:51:40 -0500

    Added comments and header

--- M02 Assignment.py
+++ M02 Assignment.py
@@ -1,4 +1,12 @@
-#Encrypt/Decrypt - Symmetric Method 
+# m02-Asst - Encrypt/Decrypt Demo 
+# author: myb
+# created: 2026-03-30  
+# IDE: visual studio code
+# program of encrypting and decrypting using symmetric and asymmetric method
+# CODING ASSISTANCE was used in this program
+
+# Encrypt/Decrypt - Symmetric Method 
+# CodeAssist-AI: Used to explain how to write encrypting and decrypting code using symmetric method
 
 #Step 1: Install cryptography library and import Fernet
 from cryptography.fernet import Fernet
@@ -17,29 +25,36 @@ encrypted_message = cipher_text.encrypt(message) #turns my message into a string
 #Step 5: Revert message back to original bytes
 plain_text = cipher_text.decrypt(encrypted_message)
 
+print()
+print()
+
 #Step 6: Decode original message
-print(f"Symmetric decryptic message: {plain_text.decode()}")
+print("Symmetric Method")
+print(f"Decryptic Message: {plain_text.decode()}")
 
 print()
 print()
 
-#Encrypt/Decrypt - Asymmetric Method 
+# Encrypt/Decrypt - Asymmetric Method 
+# CodeAssist-AI: Used to explain how to write encrypting and decrypting code using asymmetric method
 
 import rsa  
 
-# 1. Generate the Public and Private keys
+# 1: Generate the Public and Private keys
 # (pubkey is for locking, privkey is for unlocking)
 pubkey, privkey = rsa.newkeys(512)
 
-# 2. The secret message (must be converted to bytes)
+# 2: The secret message (must be converted to bytes)
 message = "For your eyes only!".encode('utf8')
 
-# 3. Encrypt the message using the Public Key
+# 3: Encrypt the message using the Public Key
 ciphertext = rsa.encrypt(message, pubkey)
 print("Asymmetric Method")
 print(f"Encrypted: {ciphertext.hex()[:50]}...")
 
-# 4. Decrypt the message using the Private Key
+# 4: Decrypt the message using the Private Key
 decrypted_message = rsa.decrypt(ciphertext, privkey)
+print(f"Decrypted Message: {decrypted_message.decode('utf8')}")
 
-print(f"Decrypted: {decrypted_message.decode('utf8')}")
+print()
+print()
