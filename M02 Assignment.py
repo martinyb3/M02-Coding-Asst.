2154474442fe247912a66b90c60b3ebc56dd4773
Martinique Akinyemi <martinique.yvette@gmail.com>
2026-03-30 22:50:19 -0500

    initial commit

---
+++ M02 Assignment.py
@@ -0,0 +1,45 @@
+#Encrypt/Decrypt - Symmetric Method 
+
+#Step 1: Install cryptography library and import Fernet
+from cryptography.fernet import Fernet
+
+#Step 2: 
+key = Fernet.generate_key() #Generate encrypted code to use as my key
+cipher_text = Fernet(key)   #prepare a cryptographic algorithm with the generated code so that it can begin producing secure outputs.
+                            #cipher_text becomes a Live Object. It is now an active "engine" that sits in your computer's memory, ready to perform tasks.
+
+#Step 3: Create the secret message; .encode() method converts the message to computer lang. (bytes and bits)
+message = "This is a secret message".encode()
+
+#Step 4: Encrypt message
+encrypted_message = cipher_text.encrypt(message) #turns my message into a string of unreadable ciphertext 
+
+#Step 5: Revert message back to original bytes
+plain_text = cipher_text.decrypt(encrypted_message)
+
+#Step 6: Decode original message
+print(f"Symmetric decryptic message: {plain_text.decode()}")
+
+print()
+print()
+
+#Encrypt/Decrypt - Asymmetric Method 
+
+import rsa  
+
+# 1. Generate the Public and Private keys
+# (pubkey is for locking, privkey is for unlocking)
+pubkey, privkey = rsa.newkeys(512)
+
+# 2. The secret message (must be converted to bytes)
+message = "For your eyes only!".encode('utf8')
+
+# 3. Encrypt the message using the Public Key
+ciphertext = rsa.encrypt(message, pubkey)
+print("Asymmetric Method")
+print(f"Encrypted: {ciphertext.hex()[:50]}...")
+
+# 4. Decrypt the message using the Private Key
+decrypted_message = rsa.decrypt(ciphertext, privkey)
+
+print(f"Decrypted: {decrypted_message.decode('utf8')}")
---
+++ README.md
@@ -0,0 +1,17 @@
+# Encryption and decryption of messages using symmetric and asymmetric methods
+
+This project is an example of how to create a VSC script that test encryption and decryption of messages using symmetric and asymmetric methods. Lines 3-21 test the symmetric method with one key, while lines 28-45 test the asymmetic method using two separate linked keys.
+
+# How to install this VSC project on GitHub
+Download the VSC link in GitHub
+In VSC go to file, then open, then open the file where the VSC file is saved
+Run the code and you should see both messages using both methods
+
+## Contact
+Name: Martinique Brown 
+Email: mbrown1110@ivytech.edu
+
+
+
+
+
